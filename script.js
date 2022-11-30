a2cec48d6b50a70d7ee713b31f818297669b9caa
Ursachianu Ionut-Alexandru <tzmic1@gmail.com>
2022-11-30 09:46:16 +0200

    Implementing the loan functionality

--- script.js
+++ script.js
@@ -191,6 +191,22 @@ btnTransfer.addEventListener(`click`, function (e) {
   console.log(amount, receiverAcc);
 });
 
+btnLoan.addEventListener(`click`, function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  // Clear input field
+  inputLoanAmount.value = ``;
+});
+
 btnClose.addEventListener(`click`, function (e) {
   e.preventDefault();
 
